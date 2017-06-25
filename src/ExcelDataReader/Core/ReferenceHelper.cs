4914639f0360d9353b9a2bf0c487e75062c099fc
andersnm <andersnm@users.noreply.github.com>
2017-06-25 14:45:24 +0200

    Made the OpenXml ReferenceHelper.ParseReference() stricter, ignores invalid references (like Excel). With test.

--- src/ExcelDataReader/Core/ReferenceHelper.cs
+++ src/ExcelDataReader/Core/ReferenceHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace ExcelDataReader.Core
 {
@@ -10,37 +11,78 @@ namespace ExcelDataReader.Core
         /// <param name="value">The value.</param>
         /// <param name="column">The column, 1-based.</param>
         /// <param name="row">The row, 1-based.</param>
-        public static void ParseReference(string value, out int column, out int row)
+        public static bool ParseReference(string value, out int column, out int row)
         {
-            // INFO: Check for a simple Solution
-            int index = ParseReference(value, out column);
+            var position = 0;
+            if (value != null)
+            {
+                while (position < value.Length)
+                {
+                    var c = value[position];
+                    if (c >= 'A' && c <= 'Z')
+                    {
+                        position++;
+                        continue;
+                    }
+                    else if (char.IsDigit(c))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        position = 0;
+                        break;
+                    }
+                }
+            }
 
-            row = int.Parse(value.Substring(index), NumberStyles.None, CultureInfo.InvariantCulture);
+            if (position == 0)
+            {
+                column = 0;
+                row = 0;
+                return false;
+            }
+
+            column = ParseColumn(value.Substring(0, position));
+
+            if (!int.TryParse(value.Substring(position), NumberStyles.None, CultureInfo.InvariantCulture, out row))
+            {
+                return false;
+            }
+
+            return true;
         }
 
         /// <summary>
-        /// Logic for the Excel dimensions. Ex: A15
+        /// Logic for the Excel column. Ex: A, XY
         /// </summary>
-        /// <param name="value">The value.</param>
-        /// <param name="column">The column, 1-based.</param>
-        /// <returns>The index of the row.</returns>
-        public static int ParseReference(string value, out int column)
+        /// <param name="value">The column string value.</param>
+        /// <returns>The column, 1-based.</returns>
+        private static int ParseColumn(string value)
         {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new FormatException("Empty column reference");
+            }
+
             int index = 0;
-            column = 0;
+            int column = 0;
 
             const int offset = 'A' - 1;
 
             for (; index < value.Length; index++)
             {
                 char c = value[index];
-                if (char.IsDigit(c))
-                    break;
+                if (c < 'A' || c > 'Z')
+                {
+                    throw new FormatException("Invalid character in column reference " + value);
+                }
+
                 column *= 26;
                 column += c - offset;
             }
 
-            return index;
+            return column;
         }
     }
 }
