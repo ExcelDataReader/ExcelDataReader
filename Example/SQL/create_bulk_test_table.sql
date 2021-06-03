cc60cdf9f2d9f0ba516c06bb4181d584ec00f8e1
cosloha <jscarletta@co.slo.ca.us>
2021-06-03 11:50:17 -0700

    add example files

---
+++ Example/SQL/create_bulk_test_table.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE dbo.bulk_test
+(
+    ID      int         NOT NULL,
+    city    varchar(60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    country varchar(60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+go
+IF OBJECT_ID(N'dbo.bulk_test') IS NOT NULL
+    PRINT N'<<< CREATED TABLE dbo.bulk_test >>>'
+ELSE
+    PRINT N'<<< FAILED CREATING TABLE dbo.bulk_test >>>'
+go
