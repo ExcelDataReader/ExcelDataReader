ec5fdb9fa0879c94fc9ec725cde99ce3313d19cf
Sebastian Hantsch <sebastian.hantsch@rokoco.com>
2025-11-26 12:02:05 +0100

    Tests for clipboard dimension

--- src/ExcelDataReader.Tests/ExcelBinaryReaderTest.cs
+++ src/ExcelDataReader.Tests/ExcelBinaryReaderTest.cs
@@ -1093,6 +1093,17 @@ public class ExcelBinaryReaderTest : ExcelTestBase
         Assert.That(dataSet.Tables[0].TableName, Is.EqualTo("List1"));
     }
 
+    [Test]
+    public void ClipboardDimension()
+    {
+        using var excelReader = ExcelReaderFactory.CreateBinaryReader(Configuration.GetTestWorkbook("Test_Clipboard_Biff8.xls"));
+
+        Assert.That(excelReader.Dimension.FromRow, Is.EqualTo(10));
+        Assert.That(excelReader.Dimension.ToRow, Is.EqualTo(11));
+        Assert.That(excelReader.Dimension.FromColumn, Is.EqualTo(6));
+        Assert.That(excelReader.Dimension.ToColumn, Is.EqualTo(7));
+    }
+
     protected override IExcelDataReader OpenReader(Stream stream, ExcelReaderConfiguration configuration = null)
     {
         return ExcelReaderFactory.CreateBinaryReader(stream, configuration);
