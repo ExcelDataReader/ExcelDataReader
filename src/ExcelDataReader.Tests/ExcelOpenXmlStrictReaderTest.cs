11bf5f95691ddb74be3ff6a3973a9c86922cd414
Johan Appelgren <johan.appelgren@gmail.com>
2026-06-18 22:05:22 +0200

    Fix strict OpenXml date-time value type

--- src/ExcelDataReader.Tests/ExcelOpenXmlStrictReaderTest.cs
+++ src/ExcelDataReader.Tests/ExcelOpenXmlStrictReaderTest.cs
@@ -22,6 +22,18 @@ public class ExcelOpenXmlStrictReaderTest : ExcelOpenXmlReaderBase
         }
     }
 
+    [Test]
+    public void Issue734_StrictDateTimeCellsReturnDateTime()
+    {
+        using IExcelDataReader reader = OpenReader("NumDoubleDateBoolString");
+        Assert.That(reader.Read(), Is.True);
+
+        var value = reader.GetValue(5);
+        Assert.That(value, Is.TypeOf<DateTime>());
+        Assert.That(reader.GetFieldType(5), Is.EqualTo(typeof(DateTime)));
+        Assert.That(reader.GetDateTime(5), Is.EqualTo((DateTime)value));
+    }
+
     protected override IExcelDataReader OpenReader(Stream stream, ExcelReaderConfiguration configuration = null)
     {
         return ExcelReaderFactory.CreateOpenXmlReader(stream, configuration);
